d25e3827a0223520cbca1ae7dc3b68665340b566
KevinxDx3 <85362446+KevinxDx3@users.noreply.github.com>
2022-07-30 20:00:32 -0500

    cambios

--- src/Principal.java
+++ src/Principal.java
@@ -2,7 +2,7 @@ import javax.swing.*;
 
 public class Principal {
     public static void main(String[] args) {
-        String entrada = JOptionPane.showInputDialog("Escriba 1 para mostrar el emoji '😀'\n"+"Escriba 2 para mostrar el emoji '🙃'\n"+"\"Escriba 3 para mostrar el emoji '😑'\n"+"4\n"+"5\n"+"6\n"+"Escriba 7 para mostrar el emoji '⛔'\n"+ "Escriba 8 para mostrar '❗'\n"+ "Escriba 9 para mostrar '☹'\n"+"Escriba 10 para mostrar el emoji '🤢'\n"+ "Escriba 11 para mostrar '🙄'\n"+"11\n"+"12\n");
+        String entrada = JOptionPane.showInputDialog("Escriba 1 para mostrar el emoji '😀'\n"+"Escriba 2 para mostrar el emoji '🙃'\n"+"\"Escriba 3 para mostrar el emoji '😑'\n"+"Escriba 4 para el emoji (Desconfiado)\n"+"Escriba 5 para el emoji Catface\n"+"6 Pacman\n"+"Escriba 7 para mostrar el emoji '⛔'\n"+ "Escriba 8 para mostrar '❗'\n"+ "Escriba 9 para mostrar '☹'\n"+"Escriba 10 para mostrar el emoji '🤢'\n"+ "Escriba 11 para mostrar '🙄'\n"+"11\n"+"escriba 12 para preocupado\n");
         int opcion=Integer.parseInt(entrada);
 
         Figuras panel =new Figuras (opcion);
